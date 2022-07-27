53ad0f9abc5b6bc510c1bf138478f884578d3c4b
eman-elaziz <eman.elaziz88@gmail.com>
2022-07-27 23:35:02 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PreheaderComponent } from './components/preheader/preheader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PreheaderComponent
   ],
   imports: [
     BrowserModule,
